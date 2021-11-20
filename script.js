221f6c623628df317ce427e17560aacdbc90a872
guthias <gmgustavom@gmail.com>
2021-11-20 20:54:10 -0300

    Function para gerar array com numeros de 1 a 4 (cores)

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ const color3 = document.getElementById('color-3');
 const color4 = document.getElementById('color-4');
 const startButton = document.getElementById('start-button');
 const points = document.getElementById('point-span');
+
+// Gerando ordem das cores
+
+let colorsOrder = [];
+
+function generateNextColor () {
+  const nextColor = 1 + Math.floor(Math.random() * 4);
+  colorsOrder.push(nextColor);
+}
