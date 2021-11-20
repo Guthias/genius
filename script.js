7720a1aa4afd2255537cd644a30e9176ca28d650
guthias <gmgustavom@gmail.com>
2021-11-20 19:54:54 -0300

    Transformando os elementos HTML em constantes JS

---
+++ script.js
@@ -0,0 +1,8 @@
+// Selecionando elementos do HTML
+
+const color1 = document.getElementById('color-1');
+const color2 = document.getElementById('color-2');
+const color3 = document.getElementById('color-3');
+const color4 = document.getElementById('color-4');
+const startButton = document.getElementById('start-button');
+const points = document.getElementById('point-span');
